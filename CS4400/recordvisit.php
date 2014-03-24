32fca2f9697fadcc4dfe3dd3471268e7fa8027a2
Justin <jcole123>
2014-03-24 12:57:58 -0400

    Added some form validation to record visity

--- CS4400/recordvisit.php
+++ CS4400/recordvisit.php
@@ -2,36 +2,56 @@
 <?php
 date_default_timezone_set('America/New_York');
 include_once 'header.php';
+
+//Record visit
+$date = $name = $sys = $dia = $diagnosis = $error = '';
+//Prescribe meds
+$drug = $notes = '';
+$dosage = $months = $days = 0;
+
+//Record visit validation
+if (isset($_POST['patname'])) {
+    $name = sanitizeString($_POST['patname']);       
+    if($name == "")
+        $error = "Must enter patient name <br/><br/>";
+}
+if (isset($_POST['dov'])) { 
+    $date = sanitizeString($_POST['dov']);
+    if($date == "")
+        $error = "Must enter a date for the visit <br/><br/>";
+}
+//TODO: Prescribe validation
+
 echo <<<_END
 <style>
 h2 {text-align:center;}
 </style>
 <h2>Record a Visit</h2>
-<form method='post' action='recordvisit.php'>
+<form method='post' action='recordvisit.php'>$error
 <span class='fieldname'>Date of Visit</span>
-<input type='date' maxlength='16' name='dov' value=''
+<input type='date' maxlength='16' name='dov' value='$date'
   /><br />
 <span class='fieldname'>Patient Name</span>
 <input type='text' maxlength='16' name='patname'
-    value='' /><br />        
+    value='$name' /><br />        
 <span class='fieldname'>Blood Pressure:</span>
 <span class='fieldname'>Systolic</span>
 <input type='text' maxlength='3' size='1' name='systolic'
-    value='' /><br />
+    value='$sys' /><br />
 <span class='fieldname'>Diastolic</span>
 <input type='text' maxlength='3' size='1' name='diastolic'
-    value='' /><br /><br />
+    value='$dia' /><br /><br />
 <span class='fieldname'>Diagnosis</span>
 <textarea rows='4' cols='50' maxlength='160' ' name='diagnosis'
-    value='' />
+    value='$diagnosis' />
 </textarea><br />
 <hr>
 <h2>Prescribe Medication</h2>
 <span class='fieldname'>Drug Name</span>
 <input type='text' maxlength='16' name='drug'
-    value='' /><br /> 
+    value='$drug' /><br /> 
 <span class='fieldname'>Dosage</span>
-<select>
+<select value="$dosage'>
   <option value="1">1</option>
   <option value="2">2</option>
   <option value="3">3</option>
@@ -43,12 +63,12 @@ h2 {text-align:center;}
 </select> per day<br/>        
 <span class='fieldname'>Duration</span>
 <input type='text' maxlength='3' size='1' name='months'
-    value='' /> months
+    value='$months' /> months
 <input type='text' maxlength='3' size='1' name='days'
     value='' /> days <br/>
 <span class='fieldname'>Notes</span>
 <textarea rows='4' cols='50' maxlength='160' ' name='notes'
-    value='' /></textarea><br/>
+    value='$days' /></textarea><br/>
 _END;
 
         
