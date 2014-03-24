a8afd22804d5d8451faac763bb6994096003aef8
Justin <jcole123>
2014-03-24 13:08:50 -0400

    Order medication basic layout.

--- CS4400/order.php
+++ CS4400/order.php
@@ -1,8 +1,46 @@
 <?php
+date_default_timezone_set('America/New_York');
+include_once 'header.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$days = $months = $dosage = 0;
+$doctor = $drug = $notes = $error = $date =  '';
+
+echo <<<_END
+<style>
+h2 {text-align:center;}
+</style>
+<h2>Order Medication</h2>$error
+<form method='post' action='order.php'>$error
+<span class='fieldname'>Drug Name</span>
+<input type='text' maxlength='16' name='drug'
+    value='$drug' /><br />
+<span class='fieldname'>Dosage</span>
+<select value="$dosage'>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+</select> per day<br/>        
+<span class='fieldname'>Duration</span>
+<input type='text' maxlength='3' size='1' name='months'
+    value='$months' /> months
+<input type='text' maxlength='3' size='1' name='days'
+    value='$days' /> days <br/>
+<span class='fieldname'>Consulting Doctor</span>
+<input type='text' name='doctor'
+    value='$doctor' /><br/><br/>
+<span class='fieldname'>Date of Visit</span>
+<input type='date' maxlength='16' name='dov' value='$date'
+_END;
+
+        
+
+?>
+
+<input type='submit' value='Checkout' />
+</form></div><br /></body></html>
 
